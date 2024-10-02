55865609ef20626b6fad0d981e92a86fecff07a9
lulzcasz <113707425+lulzcasz@users.noreply.github.com>
2024-10-02 00:57:53 -0300

    feat: implement Quill in Django (#2)

---
+++ src/quill/settings.py
@@ -0,0 +1,19 @@
+QUILL_THEME = 'snow'
+
+QUILL_TOOLBAR = [
+    [{'header': [1, 2, 3, 4, 5, 6, False]}],
+    ['bold', 'italic', 'underline', 'link'],
+    ['video', 'image'],
+    ['blockquote'],
+    [{'align': ['', 'center', 'right', 'justify']}],
+    ['code', 'code-block'],
+    [{'list': 'ordered'}, {'list': 'bullet'}],
+    ['clean'],
+]
+
+QUILL_JS = 'https://cdn.jsdelivr.net/npm/quill@2.0/dist/quill.js'
+
+QUILL_CSS = [
+    'https://cdn.jsdelivr.net/npm/quill@2.0/dist/quill.snow.css',
+    'https://cdn.jsdelivr.net/npm/quill@2.0/dist/quill.bubble.css',
+]
